a88714361e8d9682d316596f5f19a8e6fdb2ba33
aletomk <alejotomkiewicz1@gmail.com>
2023-06-16 15:28:45 -0300

    navbar terminado

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,9 +1,11 @@
 import { FaCartArrowDown } from "react-icons/fa";
 import styles from "./Cart.module.css";
+import { CartWidget } from "./CartWidget";
 
 export const Cart = () => {
-    return <div>
+    return <div className={styles.div_cart}>
       <a href="#"><FaCartArrowDown className={styles.cart} /></a>
+      <CartWidget />
     </div>
 }
 
---
+++ src/components/pages/cart/CartWidget.jsx
@@ -0,0 +1,5 @@
+import styles from "./Cart.module.css";
+
+export const CartWidget = () => {
+    return <div className={styles.cart_widget}>3</div>
+}
