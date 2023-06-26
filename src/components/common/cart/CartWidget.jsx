d73811b32a841301493d14225c76d22325446358
aletomk <alejotomkiewicz1@gmail.com>
2023-06-25 21:16:27 -0300

    preparando la home del sitio

--- src/components/common/cart/CartWidget.jsx
+++ src/components/common/cart/CartWidget.jsx
@@ -1,7 +1,7 @@
 import { FaCartArrowDown } from "react-icons/fa";
 import styles from "./Cart.module.css";
 
-export const Cart = () => {
+export const CartWidget = () => {
     return <div className={styles.div_cart}>
       <a href="#"><FaCartArrowDown className={styles.cart} /></a>
       <div className={styles.cart_widget}>3</div>
