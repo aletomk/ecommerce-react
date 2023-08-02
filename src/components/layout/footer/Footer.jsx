d4a8464382a7246ad4473581e230bb3b720ca338
aletomk <alejotomkiewicz1@gmail.com>
2023-08-02 20:58:37 -0300

    Limpieza de código y seo

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -7,41 +7,41 @@ import "./Footer.css";
 
 export const Footer = () => {
     return (
-    <footer id="grilla_footer">
-        <div className="ubicacion">
-            <h2>Donde encontrarnos</h2>
-            <h3>
-                <a href="https://www.google.com/maps" target="_blank" rel="noreferrer"><MdPlace /> Av. Constituyentes 3245 (CABA)</a>
-            </h3>
-        </div>
-        <div>
-            <h2>Contacto</h2>
-            <ul className="redes">
-                <li className="redes_item">
-                    <a href="https://www.instagram.com/alee_tomkiewicz/" target="_blank" rel="noreferrer">
-                        <InstagramIcon fontSize="large" />
-                    </a>
-                </li>
-                <li className="redes_item">
-                    <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
-                        <WhatsAppIcon fontSize="large" />
-                    </a>
-                </li>
-                <li className="redes_item">
-                    <a href="https://www.linkedin.com/in/alejo-tomkiewicz-44841a245/" target="_blank" rel="noreferrer">
-                        <LinkedInIcon fontSize="large" />
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h2><span className="faq_h2">Preguntas frequentes y</span> ayuda</h2>
-            <ul className="faq">
-                <li><a href="#">Preguntas frecuentes (FAQ)</a></li>
-                <li><a href="#"><BsQuestionOctagon /> Ayuda</a></li>
-            </ul>
-        </div>
-    </footer>    
+        <footer id="grilla_footer">
+            <div className="ubicacion">
+                <h2>Donde encontrarnos</h2>
+                <h3>
+                    <a href="https://www.google.com/maps" target="_blank" rel="noreferrer"><MdPlace /> Av. Constituyentes 3245 (CABA)</a>
+                </h3>
+            </div>
+            <div>
+                <h2>Contacto</h2>
+                <ul className="redes">
+                    <li className="redes_item">
+                        <a href="https://www.instagram.com/alee_tomkiewicz/" target="_blank" rel="noreferrer">
+                            <InstagramIcon fontSize="large" />
+                        </a>
+                    </li>
+                    <li className="redes_item">
+                        <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
+                            <WhatsAppIcon fontSize="large" />
+                        </a>
+                    </li>
+                    <li className="redes_item">
+                        <a href="https://www.linkedin.com/in/alejo-tomkiewicz-44841a245/" target="_blank" rel="noreferrer">
+                            <LinkedInIcon fontSize="large" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2><span className="faq_h2">Preguntas frequentes y</span> ayuda</h2>
+                <ul className="faq">
+                    <li><a href="#">Preguntas frecuentes (FAQ)</a></li>
+                    <li><a href="#"><BsQuestionOctagon /> Ayuda</a></li>
+                </ul>
+            </div>
+        </footer>    
     )
 }
 
